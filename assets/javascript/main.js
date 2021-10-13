e39c508049b3d28dcf5fba2b30888af8330d9591
pater97 <giuseppepater997@gmail.com>
2021-10-13 15:33:28 +0200

    condizioni fizzbuzz in console (test)

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,8 +3,21 @@
 //con uno stile differente a seconda del valore dell'indice
 // per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
-// creazione ciclo base 
+// OPERAZIONI:
 
-for (let i = 1; i <= 100; i++) {
-    console.log(i);
+// -1 creazione ciclo base 1 to 100
+// -2 verifcare tramite l'utilizzo ddell'operatore resto "%" se i multipli del numero che voglio hanno come somma 0.
+// -3 eseguire ogni condizione con if/else if/else in base a ciò che voglio ottenere
+// -4 inserire nell'html i risultati
+
+for (let i = 1; i <= 100; i++) { //1
+    if ((i % 3 == 0) && (i % 5 == 0)) { //2
+        console.log("fizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("fizz");
+    } else if (i % 5 == 0) {
+        console.log("buzz");
+    } else {
+        console.log(i);
+    }
 }
